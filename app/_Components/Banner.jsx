c852d3171eb2bde4af0d86c62d8f5554b4e34f9a
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2025-03-18 21:54:09 +0500

    Banner Section Completed

--- app/_Components/Banner.jsx
+++ app/_Components/Banner.jsx
@@ -5,57 +5,66 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="container">
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 items-center gap-20 justify-around py-24">
         <div className="p-10">
           <h1 className="text-7xl font-semibold py-5">
-            Search And Tour With <span>Ozalams</span>
+            Search And Tour With <span className="text-primary">Ozalams</span>
           </h1>
           <p className="pt-5 text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
             dolore recusandae minus ipsam mollitia distinctio sed voluptatibus
             unde. Asperiores, voluptas.
           </p>
-          <div className="flex gap-10 py-8 px-4">
+          <div className="flex gap-20 py-14 px-4">
             <Image
-              src="/assets/icon1.png"
+              src="/assets/icon2.png"
               alt="icons"
               width={100}
               height={50}
+              className="w-22 object-cover"
+            />
+            <Image
+              src="/assets/icon3.png"
+              alt="icons"
+              width={100}
+              height={50}
+              className="w-22 object-cover"
             />
             <Image
               src="/assets/icon1.png"
               alt="icons"
               width={100}
               height={50}
-            />
-            <Image
-              src="/assets/icon1.png"
-              alt="icons"
-              width={100}
-              height={50}
+              className="w-22 object-cover"
             />
           </div>
           <div className="">
-            <div className="mt-4 flex flex-col gap-2">
-              <div className="flex">
-                <input
-                  type="text"
-                  placeholder="Address, Schools City etc."
-                  className="border rounded-l-md p-4 w-full"
-                />
-                <button className="bg-blue-500 text-lg text-white rounded-r-md px-8 py-3">
-                  Search
-                </button>
-              </div>
+            <div className=" flex flex-col gap-2">
+              <form action="#" method="POST">
+                <div className="flex">
+                  <input
+                    type="text"
+                    placeholder="Address, Schools City etc."
+                    className="border rounded-l-md p-4 w-full"
+                    required
+                  />
+                  <button
+                    className="bg-primary cursor-pointer text-lg text-white rounded-r-md px-8 py-3"
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative flex justify-center">
           {/* this for postions */}
-          <div className="absolute top-5 left-5">
-            <div className="bg-gray-100/50 rounded-t-full w-40 p-3 √">
+          <div className="absolute top-5 left-16">
+            <div className="bg-gray-200/90 rounded-t-full w-40 p-3 shadow-md">
               {" "}
-              <div className=" bg-gray-800 shadow-md w-34 p-3 rounded-t-full">
+              <div className=" bg-white shadow-md w-34 p-3 rounded-t-full">
                 <div className="w-28 p-5 bg-gradient-to-r from-primary to-[#6088f4af] rounded-t-full">
                   <h1 className="text-xl pt-4 text-white font-semibold text-center">
                     Super Sale$
@@ -64,30 +73,30 @@ const Banner = () => {
               </div>
             </div>
           </div>
-          <div className="absolute right-32 top-72">
-            <div className="w-56 space-y-5 bg-gray-200/80 p-3 rounded-lg">
-              <div className="flex gap-3 items-center p-3 bg-gray-800 text-white justify-center rounded-lg">
+          <div className="absolute right-5 top-78">
+            <div className="w-56 space-y-5 bg-gray-200/60 p-3 rounded-lg">
+              <div className="flex gap-3 items-center p-3 bg-white justify-center rounded-lg">
                 <Handshake
                   size={35}
                   className="bg-primary p-2 rounded-sm text-white"
                 />
                 Paid Partner Ship
               </div>{" "}
-              <div className="flex gap-3 items-center p-3 bg-gray-800 text-white justify-center rounded-lg">
+              <div className="flex gap-3 items-center p-3 bg-white justify-center rounded-lg">
                 <Handshake
                   size={35}
                   className="bg-primary p-2 rounded-sm text-white"
                 />
                 Paid Partner Ship
               </div>{" "}
-              <div className="flex gap-3 items-center p-3 bg-gray-800 text-white justify-center rounded-lg">
+              <div className="flex gap-3 items-center p-3 bg-white justify-center rounded-lg">
                 <Handshake
                   size={35}
                   className="bg-primary p-2 rounded-sm text-white"
                 />
                 Paid Partner Ship
               </div>{" "}
-              <div className="flex gap-3 items-center p-3 bg-gray-800 text-white justify-center rounded-lg">
+              <div className="flex gap-3 items-center p-3 bg-white justify-center rounded-lg">
                 <Handshake
                   size={35}
                   className="bg-primary p-2 rounded-sm text-white"
