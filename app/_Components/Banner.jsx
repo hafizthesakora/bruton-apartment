9ab20ab6dd14621863ac2e7a64bb4331195ec3cf
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2025-03-31 15:58:15 +0500

    Install Aos Animtion npm and Initailized or Add Aos animtion on Banner.jsx

--- app/_Components/Banner.jsx
+++ app/_Components/Banner.jsx
@@ -7,10 +7,20 @@ const Banner = () => {
     <div className="container mx-auto">
       <div className="grid md:grid-cols-2 items-center gap-20 justify-around py-10 md:py-24">
         <div className="p-10">
-          <h1 className="text-4xl md:text-7xl font-semibold py-2 md:py-5">
+          <h1
+            className="text-4xl md:text-7xl font-semibold py-2 md:py-5"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out-back"
+          >
             Search And Tour With <span className="text-primary">Ozalams</span>
           </h1>
-          <p className="pt-5 text-lg">
+          <p
+            className="pt-5 text-lg"
+            data-aos="fade-right"
+            data-aos-duration="1100"
+            data-aos-easing="ease-in-out-back"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
             dolore recusandae minus ipsam mollitia distinctio sed voluptatibus
             unde. Asperiores, voluptas.
@@ -22,6 +32,9 @@ const Banner = () => {
               width={100}
               height={50}
               className="w-16 md:w-22 object-cover"
+              data-aos="fade-right"
+              data-aos-duration="1200"
+              data-aos-easing="ease-in-out-back"
             />
             <Image
               src="/assets/icon3.png"
@@ -29,6 +42,9 @@ const Banner = () => {
               width={100}
               height={50}
               className="w-16 md:w-22 object-cover"
+              data-aos="fade-right"
+              data-aos-duration="1300"
+              data-aos-easing="ease-in-out-back"
             />
             <Image
               src="/assets/icon1.png"
@@ -36,9 +52,17 @@ const Banner = () => {
               width={100}
               height={50}
               className="w-16 md:w-22 object-cover"
+              data-aos="fade-right"
+              data-aos-duration="1400"
+              data-aos-easing="ease-in-out-back"
             />
           </div>
-          <div className="">
+          <div
+            className=""
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-out-back"
+          >
             <div className=" flex flex-col gap-2">
               <form action="#" method="POST">
                 <div className="flex">
@@ -61,7 +85,12 @@ const Banner = () => {
         </div>
         <div className="relative flex justify-center">
           {/* this for postions */}
-          <div className="absolute top-8 md:top-5 left-5 md:left-16">
+          <div
+            className="absolute top-8 md:top-5 left-5 md:left-16 z-10"
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out-back"
+          >
             <div className="bg-gray-200/90 rounded-t-full w-24 md:w-40 p-2 md:p-3 shadow-md">
               {" "}
               <div className=" bg-white shadow-md w-20 md:w-34 p-2 md:p-3 rounded-t-full">
@@ -73,7 +102,12 @@ const Banner = () => {
               </div>
             </div>
           </div>
-          <div className="absolute right-3 md:right-5 top-50 md:top-78">
+          <div
+            className="absolute right-3 md:right-5 top-50 md:top-78 z-10"
+            data-aos="fade-left"
+            data-aos-duration="1800"
+            data-aos-easing="ease-in-out-back"
+          >
             <div className="w-56 space-y-5 bg-gray-200/60 p-4 md:p-3 rounded-lg">
               <div className="flex gap-3 items-center p-1 md:p-3 bg-white justify-center rounded-lg">
                 <Handshake
@@ -112,7 +146,10 @@ const Banner = () => {
               alt="home"
               width={600}
               height={600}
-              className="object-cover object-center rounded-t-full px-10"
+              className="object-cover object-center rounded-t-full px-10 -z-10"
+              data-aos="fade-down"
+              data-aos-duration="1200"
+              data-aos-easing="ease-in-out-back"
             />
           </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
+"use client";
 import Banner from "./_Components/Banner";
 import About from "./_Components/About";
 import Category from "./_Components/Category";
 import HomeFeatures from "./_Components/HomeFeatures";
 import Services from "./_Components/Services";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  }, []);
   return (
     <div className="">
       <Banner />
